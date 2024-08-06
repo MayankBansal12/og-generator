bce809b2fbc7ba2d9b89daacac104084184ee448
Mayank Bansal <mayankbansal125@gmail.com>
2024-08-07 01:42:40 +0530

    feat: add loader in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import Post from "./components/Post";
+const backend = process.env.REACT_APP_BACKEND
 
 function App() {
+  const [showLoader, setShowLoader] = useState(false)
+
+  useEffect(() => {
+    setShowLoader(true)
+    fetch(backend, {
+      method: 'GET'
+    }).then(() => {
+      setShowLoader(false)
+    })
+  }, [])
+
   return (
     <div>
-      <Post />
+      {
+        !showLoader ? <Post />
+          : <div className="loading-popup">
+            <h2>You need to wait for a min...</h2>
+            <p>Server is deployed on render, need to make sure, it is running...please wait</p>
+            <p>sorry for bad user experience</p>
+          </div>
+      }
     </div>
   );
 }
